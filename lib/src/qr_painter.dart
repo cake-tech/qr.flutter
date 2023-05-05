0478008f9eb081309a83672f7d6bf37076113d0e
Serhii <borodenko.sv@gmail.com>
2023-05-05 23:00:44 +0300

    Revert "fix padding"
    This reverts commit bac54d1814b43dc503397911b864b43a1a6fd334.

--- lib/src/qr_painter.dart
+++ lib/src/qr_painter.dart
@@ -450,7 +450,8 @@ class _PaintMetrics {
   void _calculateMetrics() {
     final gapTotal = (moduleCount - 1) * gapSize;
     var pixelSize = (containerSize - gapTotal) / moduleCount;
-    _pixelSize = pixelSize;
+    _pixelSize = (pixelSize * 2).roundToDouble() / 2;
     _innerContentSize = (_pixelSize * moduleCount) + gapTotal;
     _inset = (containerSize - _innerContentSize) / 2;
   }
+}
